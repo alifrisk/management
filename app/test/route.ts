79fcdc4f7ddba0cfc7cffdea74815ff762770fed
alifrisk <niyatorzuzoda@gmail.com>
2026-05-16 16:37:50 +0500

    Delete app directory

--- app/test/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return Response.json({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    key_start: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.substring(0, 20),
-  })
-}
