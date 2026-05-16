022056510daf18843e9682229102845f0392fffc
alifrisk <niyatorzuzoda@gmail.com>
2026-05-16 17:16:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import { redirect } from 'next/navigation'
-import { createServerClient } from '@/supabase/server'
 
-export default async function HomePage() {
-  const supabase = createServerClient()
-  const { data: { session } } = await supabase.auth.getSession()
-  if (session) {
-    redirect('/dashboard')
-  } else {
-    redirect('/auth/login')
-  }
+export default function HomePage() {
+  redirect('/dashboard')
 }
